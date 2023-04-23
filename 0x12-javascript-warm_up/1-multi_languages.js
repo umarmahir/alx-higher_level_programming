66caa24859064318d43c266c482c560859bdbf81
umarmahir <39194644+umarmahir@users.noreply.github.com>
2023-04-23 17:27:33 +0100

    Delete 0x12-javascript-warm_up directory
    I deleted the repo because I deferred my program

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-langs.forEach(function (item, index, array) { console.log(item); });
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-let n = parseInt(process.argv[2]);
-if (isNaN(n)) console.log('Not a number');
-else console.log('My number: ' + n);
