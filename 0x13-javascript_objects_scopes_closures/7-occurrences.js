e0d85d8a78d1040a12f90fbe7a4a351ee61bddf1
umarmahir <39194644+umarmahir@users.noreply.github.com>
2023-04-23 17:28:08 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  if (list === undefined || searchElement === undefined) return 0;
-  let n = 0;
-  list.forEach((element) => { if (element === searchElement) n++; });
-  return n;
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-exports.logMe = (function (item) {
-  let n = 0;
-  return function (item) { console.log(n++ + ': ' + item); };
-}());
